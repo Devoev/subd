63aec1abcdb1041569f0cbd3e5847845e855a12f
Devin <devinbalian@gmail.com>
2025-02-14 13:06:35 +0100

    update type of control points in spline.rs

--- src/bspline/spline.rs
+++ src/bspline/spline.rs
@@ -1,14 +1,16 @@
 use crate::bspline::multivariate_spline_basis::MultivariateSplineBasis;
-use nalgebra::{Point, RealField};
+use nalgebra::{Const, Dyn, OMatrix, Point, RealField};
 use std::iter::zip;
-use crate::bspline::spline_curve::SplineCurve;
+
+/// Control points of a spline. Stored column wise as a matrix.
+type ControlPoints<T, const M : usize> = OMatrix<T, Const<M>, Dyn>;
 
 /// A `D`-dimensional B-spline manifold embedded `M`-dimensional euclidian space.
 #[derive(Debug, Clone)]
 pub struct Spline<T : RealField, const D : usize, const M : usize>  {
 
     /// Control points for each parametric direction.
-    pub control_points: [Vec<Point<T, M>>; D], // todo: replace with ndarray or matrix?
+    pub control_points: ControlPoints<T, M>,
 
     /// B-spline basis functions for the parametrization.
     pub basis: MultivariateSplineBasis<T, D>
@@ -17,12 +19,13 @@ pub struct Spline<T : RealField, const D : usize, const M : usize>  {
 impl<T : RealField, const D : usize, const M : usize> Spline<T, D, M> {
 
     /// Returns `true` if there are the same amount of control points as basis functions.
-    fn coeffs_match_basis(control_points: &[Vec<Point<T, M>>; D], basis: &MultivariateSplineBasis<T, D>) -> bool {
-        zip(control_points, &basis.univariate_bases).all(|(c, b)| c.len() == b.n)
+    fn coeffs_match_basis(control_points: &ControlPoints<T, M>, basis: &MultivariateSplineBasis<T, D>) -> bool {
+        // zip(control_points, &basis.univariate_bases).all(|(c, b)| c.len() == b.n)
+        todo!("Unflatten control points")
     }
 
     /// Constructs a new [`Spline`].
-    pub fn new(control_points: [Vec<Point<T, M>>; D], basis: MultivariateSplineBasis<T, D>) -> Option<Self> {
+    pub fn new(control_points: ControlPoints<T, M>, basis: MultivariateSplineBasis<T, D>) -> Option<Self> {
         if Self::coeffs_match_basis(&control_points, &basis) {
             Some(Spline { control_points, basis })
         }
@@ -34,11 +37,12 @@ impl<T : RealField + Copy, const M : usize> Spline<T, 1, M> {
 
     /// Evaluates the spline curve at the parametric point `t`.
     pub fn eval(&self, t: T) -> Point<T, M> {
-        let basis = &self.basis.univariate_bases[0];
-        let idx = basis.find_span(t).unwrap();
-        let b = self.basis.eval(t);
-        let c = &self.control_points[0][idx - basis.p..=idx];
-        zip(&b, c).fold(Point::origin(), |pos, (bi, ci)| pos + ci.coords * *bi)
+        // let basis = &self.basis.univariate_bases[0];
+        // let idx = basis.find_span(t).unwrap();
+        // let b = self.basis.eval(t);
+        // let c = &self.control_points[0][idx - basis.p..=idx];
+        // zip(&b, c).fold(Point::origin(), |pos, (bi, ci)| pos + ci.coords * *bi)
+        todo!("Fix control points type")
     }
 }
 
