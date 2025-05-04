cb40d2f2e9df295fd315ce1c6d9fdce83bdc743b
Devin <devinbalian@gmail.com>
2025-05-04 23:05:12 +0200

    add docs to connected_to method in cell.rs

--- src/mesh/cell.rs
+++ src/mesh/cell.rs
@@ -7,8 +7,29 @@ pub trait CellTopo<K: DimName> {
     /// Returns a slice of all node indices in a mesh corresponding to the corner vertices of the cell.
     fn nodes(&self) -> &[VertexTopo];
 
-    /// Returns `true` if this cell is connected to the `other` cell
-    /// by an [`M`]-dimensional (sub-)cell with `M <= K`.
+    /// Returns `true` if this cell is topologically connected (or adjacent) to the `other` cell
+    /// by an [`M`]-dimensional sub-cell with `M <= K`. 
+    /// That is if the two cells share a common `M`-cell (up to node ordering and orientation).
+    /// 
+    /// # Examples
+    /// For quadrilateral faces (`K = 2`) the following faces are connected
+    /// ```text
+    /// +---+---+
+    /// |   |   |
+    /// +---+---+
+    /// ```
+    /// by an edge (`M = 1`) and `connected_to<1>` returns `true`.
+    /// 
+    /// The faces
+    /// ```text
+    /// +---+
+    /// |   |
+    /// +---+---+
+    ///     |   |
+    ///     +---+
+    /// ```
+    /// are not connected by an edge, but by a node (`M = 0`).
+    /// In this case `connected_to<1>` returns `false` but `connected_to<0>` returns `true`.
     fn connected_to<const M: usize>(&self, other: &Self) -> bool
         where K: DimNameSub<Const<M>>;
     
