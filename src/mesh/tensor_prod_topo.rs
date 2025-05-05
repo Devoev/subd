11e9a0b7819b32b7de4c11aa6ee9b8dc1b408b92
Devin <devinbalian@gmail.com>
2025-05-05 22:01:51 +0200

    start implementation of tensor_prod_topo.rs

--- src/mesh/tensor_prod_topo.rs
+++ src/mesh/tensor_prod_topo.rs
@@ -1,9 +1,29 @@
 //! Topology of a tensor product mesh.
 
-use crate::cells::vertex::VertexTopo;
+use crate::knots::index::Strides;
 
-// todo: rethink this
 pub struct TensorProd<const K: usize> {
-    /// Breakpoints for each parametric direction.
-    pub breaks: [Vec<VertexTopo>; K]
+    /// Strides for each parametric direction.
+    strides: Strides<usize, K>
+}
+
+impl<const K: usize> TensorProd<K> {
+    /// Constructs a new [`TensorProd`] from the given `strides`.
+    pub fn new(strides: Strides<usize, K>) -> Self {
+        TensorProd { strides }
+    }
+}
+
+impl TensorProd<3> {
+    /// Constructs a new [`TensorProd`] from the given dimensions `nx`, `ny` and `nz`.
+    pub fn from_dims(nx: usize, ny: usize, nz: usize) -> Self {
+        TensorProd::new(Strides::from_dims([nx, ny, nz]))
+    }
+}
+
+impl TensorProd<2> {
+    /// Constructs a new [`TensorProd`] from the given dimensions `nx` and `ny`.
+    pub fn from_dims(nx: usize, ny: usize, nz: usize) -> Self {
+        TensorProd::new(Strides::from_dims([nx, ny]))
+    }
 }
