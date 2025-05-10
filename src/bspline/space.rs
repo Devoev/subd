68acd4afd6e88c5be5e226288bc8243d57dfc3f6
devin <devinbalian@gmail.com>
2025-05-10 12:42:26 +0200

    add eval_coeffs to space.rs

--- src/bspline/space.rs
+++ src/bspline/space.rs
@@ -1,6 +1,7 @@
-use std::marker::PhantomData;
-use nalgebra::RealField;
 use crate::bspline::basis::BsplineBasis;
+use crate::bspline::control_points::OControlPoints;
+use nalgebra::{Dyn, RealField, SVector};
+use std::marker::PhantomData;
 
 /// Function space spanned by the B-Spline basis functions.
 #[derive(Debug, Clone)]
@@ -20,5 +21,13 @@ impl <T: RealField, X, B: BsplineBasis<T, X>> BsplineSpace<T, X, B> {
     /// Returns the dimension of this space, i.e. the number of basis functions in its basis.
     pub fn dim(&self) -> usize {
         self.basis.len()
+    }
+
+    /// Evaluates the B-Spline function of this space represented by the coefficients `c`
+    /// at the parametric point `x`.
+    pub fn eval_coeffs<const D: usize>(&self, c: OControlPoints<T, D, Dyn>, x: X) -> SVector<T, D> {
+        let (b, idx) = self.basis.eval_nonzero(x);
+        let c = c.get_nonzero(idx);
+        c.coords * b
     }
 }
