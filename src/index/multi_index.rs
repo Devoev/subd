80ed34adf4050031dad181a4a00bb751ef705f0c
devin <devinbalian@gmail.com>
2025-05-12 11:32:09 +0200

    add Prod struct in tensor_prod.rs impl Dimensioned and MultiIndex for tuples in multi_index.rs

--- src/index/multi_index.rs
+++ src/index/multi_index.rs
@@ -15,15 +15,41 @@ impl Dimensioned<1, usize> for usize {
     }
 }
 
-impl MultiIndex<1, usize> for usize {
-    fn into_lin(self, _: Strides<1>) -> usize {
-        self
+impl Dimensioned<2, usize> for (usize, usize) {
+    fn into_arr(self) -> [usize; 2] {
+        [self.0, self.1]
+    }
+}
+
+impl Dimensioned<3, usize> for (usize, usize, usize) {
+    fn into_arr(self) -> [usize; 3] {
+        [self.0, self.1, self.2]
     }
 }
 
 impl<const D: usize> Dimensioned<D, usize> for [usize; D] {
     fn into_arr(self) -> [usize; D] {
         self
+    }
+}
+
+impl MultiIndex<1, usize> for usize {
+    fn into_lin(self, _: Strides<1>) -> usize {
+        self
+    }
+}
+
+impl MultiIndex<2, usize> for (usize, usize) {
+    fn into_lin(self, strides: Strides<2>) -> usize {
+        let strides = strides.into_arr();
+        self.0 * strides[0] + self.1 * strides[1]
+    }
+}
+
+impl MultiIndex<3, usize> for (usize, usize, usize) {
+    fn into_lin(self, strides: Strides<3>) -> usize {
+        let strides = strides.into_arr();
+        self.0 * strides[0] + self.1 * strides[1] + self.2 * strides[2]
     }
 }
 
