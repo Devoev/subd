18fb4358727f69628f7a750d31a0936281a66090
devin <devinbalian@gmail.com>
2025-05-15 13:38:00 +0200

    update cart_prod.rs eval_nonzero impl

--- src/bspline/cart_prod.rs
+++ src/bspline/cart_prod.rs
@@ -1,5 +1,5 @@
 use std::marker::PhantomData;
-use nalgebra::{Dyn, OMatrix, RealField, U2};
+use nalgebra::{stack, Dyn, OMatrix, RealField, U2};
 use crate::bspline::basis::BsplineBasis;
 
 /// Cartesian product of two scalar bases [`B1`] and [`B2`].
@@ -14,6 +14,7 @@ pub struct Prod<T: RealField, X, B1: BsplineBasis<T, X>, B2: BsplineBasis<T, X>>
     _phantom_data: PhantomData<(T, X)>,
 }
 
+#[allow(clippy::toplevel_ref_arg)]
 impl <T: RealField, X: Copy, B1: BsplineBasis<T, X>, B2: BsplineBasis<T, X>> Prod<T, X, B1, B2> {
     /// Constructs a new [`Prod`] from the bases `b1` and `b2`.
     pub fn new(b1: B1, b2: B2) -> Self {
@@ -23,8 +24,7 @@ impl <T: RealField, X: Copy, B1: BsplineBasis<T, X>, B2: BsplineBasis<T, X>> Pro
     pub fn eval_nonzero(&self, x: X) -> OMatrix<T, Dyn, U2> {
         let (b1, _) = self.b1.eval_nonzero(x);
         let (b2, _) = self.b2.eval_nonzero(x);
-        println!("{b1}");
-        println!("{b2}");
-        OMatrix::from_columns(&[b1, b2])
+        stack![b1, 0; 
+               0, b2]
     }
 }
