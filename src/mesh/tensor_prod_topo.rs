e619a9e1b234d2b3d9e4bec275398784e6235769
devin <devinbalian@gmail.com>
2025-06-18 14:02:18 +0200

    add named NodesIter and ElemsIter types in tensor_prod_topo.rs

--- src/mesh/tensor_prod_topo.rs
+++ src/mesh/tensor_prod_topo.rs
@@ -32,7 +32,7 @@ pub struct TensorProd<const D: usize> {
     pub strides: Strides<D>
 }
 
-impl<const D: usize> TensorProd<D> {
+impl <const D: usize> TensorProd<D> {
     /// Constructs a new [`TensorProd`] from the given `dim_shape` and `strides`.
     pub fn new(dim_shape: DimShape<D>, strides: Strides<D>) -> Self {
         TensorProd { dim_shape, strides }
@@ -42,20 +42,50 @@ impl<const D: usize> TensorProd<D> {
     pub fn from_dims(dim_shape: DimShape<D>) -> Self {
         TensorProd::new(dim_shape, Strides::from(dim_shape))
     }
-    
+}
+
+/// An iterator over the nodes of a [`TensorProd<D>`] mesh.
+pub struct NodesIter<'a, const D: usize> {
+    iter: MultiRange<[usize; D]>,
+    strides: &'a Strides<D>
+}
+
+impl <'a, const D: usize> NodesIter<'a, D> {
+    /// Constructs a new [`NodesIter`] from the given `iter` and `strides`.
+    pub fn new(iter: MultiRange<[usize; D]>, strides: &'a Strides<D>) -> Self {
+        NodesIter { iter, strides }
+    }
+
+    /// Constructs a enw [`NodesIter`] from the given tensor-product `msh`.
+    pub fn from_msh(msh: &'a TensorProd<D>) -> Self {
+        NodesIter::new(msh.indices(), &msh.strides)
+    }
+}
+
+impl<const D: usize> Iterator for NodesIter<'_, D> {
+    type Item = NodeIdx;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter.next().map(|idx| NodeIdx(idx.into_lin(self.strides)))
+    }
+}
+
+/// An iterator over the elements of a [`TensorProd<D>`] mesh.
+pub type ElemsIter<const D: usize> = Map<MultiRange<[usize; D]>, fn([usize; D]) -> HyperRectangleTopo<D>>;
+
+impl<const D: usize> TensorProd<D> {
     /// Returns an iterator over all multi-indices in this grid.
-    pub fn indices(&self) -> MultiRange<[usize; D]>{
+    pub fn indices(&self) -> MultiRange<[usize; D]> {
         self.dim_shape.multi_range()
     }
-    
-    // todo: replace iterator type with newtype NodesIter
+
     /// Returns an iterator over all nodes with linear indices in increasing index order.
-    pub fn nodes(&self) -> Map<MultiRange<[usize; D]>, impl FnMut([usize; D]) -> NodeIdx + '_> {
-        self.indices().map(|idx| NodeIdx(idx.into_lin(&self.strides)))
+    pub fn nodes(&self) -> NodesIter<'_, D> {
+        NodesIter::from_msh(self)
     }
 
     /// Returns an iterator over all elements in lexicographical order.
-    pub fn elems(&self) -> impl Iterator<Item=HyperRectangleTopo<D>> {
+    pub fn elems(&self) -> ElemsIter<D> {
         let mut dim_shape_elems = self.dim_shape;
         dim_shape_elems.shrink(1);
         dim_shape_elems.multi_range().map(HyperRectangleTopo)
@@ -63,8 +93,8 @@ impl<const D: usize> TensorProd<D> {
 }
 
 impl<'a, const D: usize> MeshTopology<'a, D, HyperRectangleTopo<D>> for TensorProd<D> {
-    type Nodes = impl Iterator<Item =NodeIdx>;
-    type Elems = impl Iterator<Item = HyperRectangleTopo<D>>;
+    type Nodes = NodesIter<'a, D>;
+    type Elems = ElemsIter<D>;
 
     fn num_nodes(&self) -> usize {
         self.dim_shape.len()
