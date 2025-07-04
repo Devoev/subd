8d43b44897519a3ed11582b1f421b46118b9e0b6
devin <devinbalian@gmail.com>
2025-07-04 17:16:07 +0200

    add implement interpolating evaluation in quadratic.rs

--- src/bspline/quadratic.rs
+++ src/bspline/quadratic.rs
@@ -1,4 +1,4 @@
-use nalgebra::{matrix, Matrix3, RealField, RowSVector, RowVector3, RowVector4};
+use nalgebra::{matrix, Matrix2, Matrix3, RealField, RowSVector, RowVector2, RowVector3, RowVector4};
 use std::sync::LazyLock;
 
 /// The `3✕3` basis transformation matrix for [`QuadraticBspline::Smooth`].
@@ -7,6 +7,14 @@ static MAT_SMOOTH: LazyLock<Matrix3<f64>> = LazyLock::new(|| {
         1, 1, 0;
         -2, 2, 0;
         1, -2, 1;
+    ].cast() / 2.0
+});
+
+/// The `2✕2` basis transformation matrix for [`QuadraticBspline::Interpolating`].
+static MAT_INTERPOLATING: LazyLock<Matrix2<f64>> = LazyLock::new(|| {
+    matrix![
+        -1, 1;
+        -2, 0;
     ].cast() / 2.0
 });
 
@@ -26,5 +34,11 @@ impl QuadraticBspline {
     pub fn eval_smooth<T: RealField + Copy>(x: T) -> RowVector3<T> {
         let pows = matrix![x.powi(2), x, T::one()];
         pows * MAT_SMOOTH.cast()
+    }
+    /// Evaluates the `2` interpolating quadratic B-Splines at `x`
+    /// as the row-vector `(b[1],b[2])`.
+    pub fn eval_interpolating<T: RealField + Copy>(x: T) -> RowVector2<T> {
+        let pows = matrix![x.powi(2), T::one()];
+        pows * MAT_INTERPOLATING.cast()
     }
 }
