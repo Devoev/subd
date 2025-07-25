7a8864f8903b90c16dcf3003492e480fd3a50d25
Devin <devinbalian@gmail.com>
2025-07-25 16:51:42 +0200

    add convergence study in catmull_clark_diffusion_reaction_neu_square.rs updated expected rate in error_plot.m

--- examples/catmull_clark_diffusion_reaction_neu_square.rs
+++ examples/catmull_clark_diffusion_reaction_neu_square.rs
@@ -7,9 +7,12 @@
 //! ```
 //! with `Ω=(0,1)²` being the unit square.
 
-use std::f64::consts::PI;
 use nalgebra::{matrix, DVector, Point2, Vector1};
 use nalgebra_sparse::CsrMatrix;
+use std::f64::consts::PI;
+use std::io;
+use std::iter::zip;
+use std::process::Command;
 use subd::cells::quad::QuadNodes;
 use subd::cg::cg;
 use subd::mesh::face_vertex::QuadVertexMesh;
@@ -23,7 +26,10 @@ use subd::subd::catmull_clark::basis::CatmarkBasis;
 use subd::subd::catmull_clark::mesh::CatmarkMesh;
 use subd::subd::catmull_clark::space::CatmarkSpace;
 
-pub fn main() {
+/// Number of refinements for the convergence study.
+const NUM_REFINE: u8 = 5;
+
+pub fn main() -> io::Result<()> {
     // Define problem
     let u = |p: Point2<f64>| Vector1::new((p.x * PI).cos() * (p.y * PI).cos());
     let f = |p: Point2<f64>| (2.0 * PI.powi(2) + 1.0) * u(p);
@@ -35,12 +41,51 @@ pub fn main() {
 
     // Define mesh
     let quads = vec![QuadNodes::from_indices(0, 1, 2, 3)];
-    let quad_msh = QuadVertexMesh::from_matrix(coords_square, quads);
-    let refined = quad_msh.catmark_subd().catmark_subd().unpack();
-    let msh = CatmarkMesh::from_quad_mesh(refined);
+    let mut quad_msh = QuadVertexMesh::from_matrix(coords_square, quads);
 
+    // Convergence study
+    let mut n_dofs = vec![];
+    let mut errs = vec![];
+    for i in 0..NUM_REFINE {
+        // Print info
+        println!("Iteration {} / {NUM_REFINE}", i+1);
+
+        // Refine and construct Catmark mesh
+        quad_msh = quad_msh.catmark_subd().unpack();
+        let msh = CatmarkMesh::from_quad_mesh(quad_msh.clone());
+
+        // Solve problem
+        let (n_dof, err_l2, norm_l2) = solve(&msh, u, f);
+
+        // Save and print
+        n_dofs.push(n_dof);
+        errs.push(err_l2);
+        println!("  Absolute L2 error ||u - u_h||_2 = {:.7}", err_l2);
+        println!("  Relative L2 error ||u - u_h||_2 / ||u||_2 = {:.5}%", err_l2 / norm_l2 * 100.0);
+    }
+
+    // Write data
+    let mut writer = csv::Writer::from_path("examples/err_catmull_clark.csv")?;
+    writer.write_record(&["n_dofs", "err_l2"])?;
+    for data in zip(n_dofs, errs) {
+        writer.serialize(data)?;
+    }
+    writer.flush()?;
+
+    // Call octave plotting function
+    Command::new("octave")
+        .arg("error_plot.m")
+        .current_dir("examples/")
+        .output()?;
+
+    Ok(())
+}
+
+/// Solves the problem with right hand side `f` and solution `u` on the given `msh`.
+/// Returns the number of DOFs, the L2 error, and the relative L2 error.
+fn solve(msh: &CatmarkMesh<f64, 2>, u: impl Fn(Point2<f64>) -> Vector1<f64>, f: impl Fn(Point2<f64>) -> Vector1<f64>) -> (usize, f64, f64) {
     // Define space
-    let basis = CatmarkBasis(&msh);
+    let basis = CatmarkBasis(msh);
     let space = CatmarkSpace::new(basis);
 
     // Define quadrature
@@ -49,9 +94,9 @@ pub fn main() {
     let quad = PullbackQuad::new(ref_quad);
 
     // Assemble system
-    let f = assemble_function(&msh, &space, quad.clone(), f);
-    let hodge = Hodge::new(&msh, &space);
-    let laplace = Laplace::new(&msh, &space);
+    let hodge = Hodge::new(msh, &space);
+    let laplace = Laplace::new(msh, &space);
+    let f = assemble_function(msh, &space, quad.clone(), f);
     let m_coo = hodge.assemble(quad.clone());
     let k_coo = laplace.assemble(quad.clone());
     let m = CsrMatrix::from(&m_coo);
@@ -63,9 +108,7 @@ pub fn main() {
     // Calculate error
     let u = DVector::from_iterator(msh.num_nodes(), msh.coords.iter().map(|&p| u(p).x));
     let du = &u - uh;
-    let err_l2 = (&m * &du).dot(&du);
-    let norm_l2 = (&m * &u).dot(&u);
-
-    println!("Absolute L2 error ||u - u_h||_2 = {:.7}", err_l2);
-    println!("Relative L2 error ||u - u_h||_2 / ||u||_2 = {:.5}%", err_l2 / norm_l2 * 100.0);
+    let err_l2 = (&m * &du).dot(&du).sqrt();
+    let norm_l2 = (&m * &u).dot(&u).sqrt();
+    (msh.num_nodes(), err_l2, norm_l2)
 }
--- examples/error_plot.m
+++ examples/error_plot.m
@@ -5,8 +5,8 @@ err_l2 = data(:,2);
 figure()
 loglog(n_dof, err_l2, 'x-')
 hold on
-fplot(@(n) n.^(-3/2), [n_dof(1) n_dof(end)], 'r--')
+fplot(@(n) n.^(-4/2), [n_dof(1) n_dof(end)], 'r--')
 xlabel("Number of DOFs")
-ylabel("$L^2$ error")
-legend("Data", "Expected rate (3)")
+ylabel("L^2 error")
+legend("Data", "Expected rate in L^2 (p + 1 = 4)")
 pause()
