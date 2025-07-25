3919667ab7de2495bb6b7ef4c6084466b4935f68
Devin <devinbalian@gmail.com>
2025-07-25 17:13:39 +0200

    change error_plot.m to plot against sqrt(n_dof)

--- examples/error_plot.m
+++ examples/error_plot.m
@@ -1,12 +1,12 @@
 data = csvread("err_catmull_clark.csv", 1, 0);
-n_dof = data(:,1);
+n_dof_sqrt = sqrt(data(:,1));
 err_l2 = data(:,2);
 
 figure()
-loglog(n_dof, err_l2, 'x-')
+loglog(n_dof_sqrt, err_l2, 'x-')
 hold on
-fplot(@(n) n.^(-4/2), [n_dof(1) n_dof(end)], 'r--')
-xlabel("Number of DOFs")
+fplot(@(n) n.^(-4), [n_dof_sqrt(1) n_dof_sqrt(end)], 'r--')
+xlabel("Square root of number of DOFs")
 ylabel("L^2 error")
 legend("Data", "Expected rate in L^2 (p + 1 = 4)")
 pause()
