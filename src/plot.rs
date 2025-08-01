c4cdac443aa52040da7b35959e9e6e4eeab86a4b
Devin <devinbalian@gmail.com>
2025-08-01 11:49:00 +0200

    add plot_nodes, plot_solution_elem and plot_solution functions in plot.rs

--- src/plot.rs
+++ src/plot.rs
@@ -1,8 +1,19 @@
-use plotly::{Layout, Plot, Scatter};
+use iter_num_tools::lin_space;
+use itertools::Itertools;
+use plotly::{Layout, Plot, Scatter, Surface};
+use plotly::common::{ColorScale, ColorScalePalette};
 use plotly::layout::Annotation;
+use crate::basis::lin_combination::LinCombination;
+use crate::cells::geo::Cell;
 use crate::cells::line_segment::LineSegment;
+use crate::cells::node::NodeIdx;
 use crate::cells::quad::{Quad, QuadNodes};
+use crate::diffgeo::chart::Chart;
 use crate::mesh::face_vertex::QuadVertexMesh;
+use crate::mesh::traits::Mesh;
+use crate::subd::catmull_clark::basis::CatmarkBasis;
+use crate::subd::catmull_clark::mesh::CatmarkMesh;
+use crate::subd::catmull_clark::patch::{CatmarkPatch, CatmarkPatchNodes};
 
 /// Plots the given `faces` of a 2D quad-vertex `msh`.
 pub fn plot_faces(msh: &QuadVertexMesh<f64, 2>, faces: impl Iterator<Item=QuadNodes>) -> Plot {
@@ -27,5 +38,71 @@ pub fn plot_faces(msh: &QuadVertexMesh<f64, 2>, faces: impl Iterator<Item=QuadNo
     }
 
     plot.set_layout(layout);
+    plot
+}
+
+/// Plots the given `nodes` of a 2D quad-vertex `msh`.
+pub fn plot_nodes(msh: &QuadVertexMesh<f64, 2>, nodes: impl Iterator<Item=NodeIdx>) -> Plot {
+    let mut plot = Plot::new();
+    let mut layout = Layout::new();
+
+    for (num, node) in nodes.enumerate() {
+        let pos = msh.coords(node);
+        let pos_trace = Scatter::new(vec![pos.x], vec![pos.y]);
+        plot.add_trace(pos_trace);
+
+        let text = Annotation::new()
+            .text(num.to_string())
+            .show_arrow(false)
+            .x(pos.x)
+            .y(pos.y + 0.05);
+        layout.add_annotation(text);
+    }
+
+    plot.set_layout(layout);
+    plot
+}
+
+/// Plots the discrete solution function `uh` on the given `elem`  of the `msh`
+/// using `num` evaluation points per parametric direction.
+pub fn plot_solution_elem(msh: &CatmarkMesh<f64, 2>, elem: &CatmarkPatchNodes, uh: &LinCombination<f64, (f64, f64), CatmarkBasis<f64, 2>, 2>, num: usize) -> Plot {
+    let mut plot = Plot::new();
+    let u_range = lin_space(0.0..=1.0, num);
+    let v_range = u_range.clone();
+
+    let mut x = vec![vec![0.0; num]; num];
+    let mut y = vec![vec![0.0; num]; num];
+    let mut z = vec![vec![0.0; num]; num];
+
+    let mapping = msh.geo_elem(elem).geo_map();
+    for (i, u) in u_range.clone().enumerate() {
+        for (j, v) in v_range.clone().enumerate() {
+            // Evaluate patch
+            let pos = mapping.eval((u, v));
+
+            // Set coordinates
+            x[i][j] = pos.x;
+            y[i][j] = pos.y;
+            z[i][j] = uh.eval_on_elem(&elem, (u, v)).x;
+        }
+    }
+
+    let surface = Surface::new(z).x(x).y(y)
+        .color_scale(ColorScale::Palette(ColorScalePalette::Viridis))
+        .show_scale(false);
+    plot.add_trace(surface.clone());
+    plot
+}
+
+/// Plots the discrete solution function `uh` on the entire `msh`
+/// using `num` evaluation points per parametric direction per element.
+pub fn plot_solution(msh: &CatmarkMesh<f64, 2>, uh: &LinCombination<f64, (f64, f64), CatmarkBasis<f64, 2>, 2>, num: usize) -> Plot {
+    let mut plot = Plot::new();
+
+    for elem in &msh.elems {
+        let elem_plt = plot_solution_elem(msh, elem, uh, num);
+        plot.add_traces(elem_plt.data().iter().cloned().collect_vec());
+    }
+
     plot
 }
