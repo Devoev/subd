84d74bf5e1117e1616b8891ae7ec5bd9425db6b2
devin <devinbalian@gmail.com>
2025-09-24 10:41:56 +0200

    use evaluation of pl basis on fine level in subd_0_form_matrix.rs

--- examples/subd_0_form_matrix.rs
+++ examples/subd_0_form_matrix.rs
@@ -2,6 +2,7 @@ use nalgebra::{center, matrix, point, DMatrix, Point2, RowDVector};
 use nalgebra_sparse::CsrMatrix;
 use std::f64::consts::PI;
 use rand::random_range;
+use subd::basis::space::Space;
 use subd::cells::quad::QuadNodes;
 use subd::mesh::face_vertex::QuadVertexMesh;
 use subd::mesh::traits::MeshTopology;
@@ -13,6 +14,7 @@ use subd::subd::catmull_clark::matrices::assemble_global_mat;
 use subd::subd::catmull_clark::mesh::CatmarkMesh;
 use subd::subd::catmull_clark::quadrature::SubdUnitSquareQuad;
 use subd::subd::catmull_clark::space::CatmarkSpace;
+use subd::subd::lin_subd::basis::PlBasisQuad;
 
 fn main() {
     // Define geometry
@@ -23,7 +25,7 @@ fn main() {
 
     // Define coarse mesh
     let quads = vec![QuadNodes::from_indices(0, 1, 2, 3)];
-    let mut msh = QuadVertexMesh::from_matrix(coords_square, quads).lin_subd().unpack();
+    let mut msh = QuadVertexMesh::from_matrix(coords_square, quads).lin_subd().lin_subd().lin_subd().unpack();
     // // Define geometry
     // let coords = make_geo(1.0, 5);
     //
@@ -40,7 +42,7 @@ fn main() {
     let space_catmark_coarse = CatmarkSpace::new(CatmarkBasis(&msh_catmark_coarse));
 
     // Refine mesh and calculate subdivision matrix
-    let num_refine = 1;
+    let num_refine = 3;
     let mut a = CsrMatrix::identity(msh.num_nodes());
     for _ in 0..num_refine {
         // Update subdivision matrix
@@ -54,37 +56,41 @@ fn main() {
     }
 
     // Define space
-    // let space_pl = Space::<f64, _, 2>::new(PlBasisQuad(&msh));
+    let space_pl = Space::<f64, _, 2>::new(PlBasisQuad(&msh));
 
     // Define quadrature
     let ref_quad = GaussLegendreBi::with_degrees(4, 4);
-    // let quad = PullbackQuad::new(ref_quad.clone());
+    let quad = PullbackQuad::new(ref_quad.clone());
     let quad_catmark = PullbackQuad::new(SubdUnitSquareQuad::new(ref_quad, 10));
 
     // Build DEC mass matrix on refined mesh
-    // let mass_matrix_dec = Hodge::new(&msh, &space_pl).assemble(quad);
-    // let m_dec_fine = CsrMatrix::from(&mass_matrix_dec);
+    let mass_matrix_dec = Hodge::new(&msh, &space_pl).assemble(quad);
+    let m_dec_fine = CsrMatrix::from(&mass_matrix_dec);
 
     // todo: using the exact catmull-clark matrix on finer level.
     //  This should technically be EXACTLY the matrix of the coarser level,
     //  by the refinability property, but it isn't...
-    let msh_catmark_fine = CatmarkMesh::from(msh.clone());
-    let space_catmark_fine = CatmarkSpace::new(CatmarkBasis(&msh_catmark_fine));
-    let m_dec_fine = Hodge::new(&msh_catmark_fine, &space_catmark_fine).assemble(quad_catmark.clone());
-    let m_dec_fine = CsrMatrix::from(&m_dec_fine);
+    // let msh_catmark_fine = CatmarkMesh::from(msh.clone());
+    // let space_catmark_fine = CatmarkSpace::new(CatmarkBasis(&msh_catmark_fine));
+    // let m_dec_fine = Hodge::new(&msh_catmark_fine, &space_catmark_fine).assemble(quad_catmark.clone());
+    // let m_dec_fine = CsrMatrix::from(&m_dec_fine);
 
     // Element for single basis evaluation
     let uv_fine = (random_range(0.0..1.0), random_range(0.0..1.0));
     let uv_coarse = (uv_fine.0 / 2_i32.pow(num_refine) as f64, uv_fine.1 / 2_i32.pow(num_refine) as f64);
-    let elem_fine = &msh_catmark_fine.elems[0];
+    // let elem_fine = &msh_catmark_fine.elems[0];
+    let elem_pl = &msh.elems[0];
     let elem_coarse = &msh_catmark_coarse.elems[0];
 
     // Calculate SEC on initial mesh by using the subdivision of basis functions
-    let mut b_fine = RowDVector::zeros(msh_catmark_fine.num_nodes());
-    space_catmark_fine.populate_global_on_elem(&mut b_fine, &elem_fine, uv_fine);
+    let mut b_pl = RowDVector::zeros(msh.num_nodes());
+    space_pl.populate_global_on_elem(&mut b_pl, &elem_pl, uv_fine);
+    // let mut b_fine = RowDVector::zeros(msh_catmark_fine.num_nodes());
+    // space_catmark_fine.populate_global_on_elem(&mut b_fine, &elem_fine, uv_fine);
 
     let m_sec_coarse = a.transpose() * m_dec_fine * &a;
-    let b_sec = (a.transpose() * b_fine.transpose()).transpose();
+    let b_sec = (a.transpose() * b_pl.transpose()).transpose();
+    // let b_sec = (a.transpose() * b_fine.transpose()).transpose();
     let mass_matrix_sec = DMatrix::from(&m_sec_coarse);
 
     // Calculate catmull-clark mass matrix directly on initial mesh
@@ -95,11 +101,11 @@ fn main() {
     let mass_matrix_cc = DMatrix::from(&m_cc_coarse);
 
     // println!("{:?}", mass_matrix_catmark.shape());
-    println!("SEC basis evaluation = {}", b_sec);
-    println!("Direct basis evaluation = {}", b_coarse);
-    println!("SEC mass matrix = {}", mass_matrix_sec);
-    println!("Direct mass matrix = {}", mass_matrix_cc);
-    println!("Difference = {}", &mass_matrix_sec - &mass_matrix_cc);
+    // println!("SEC basis evaluation = {}", b_sec);
+    // println!("Direct basis evaluation = {}", b_coarse);
+    // println!("SEC mass matrix = {}", mass_matrix_sec);
+    // println!("Direct mass matrix = {}", mass_matrix_cc);
+    // println!("Difference = {}", &mass_matrix_sec - &mass_matrix_cc);
     println!("Relative error = {:e} %", (mass_matrix_sec - &mass_matrix_cc).norm() / mass_matrix_cc.norm() * 100.0);
     println!("Relative error of basis evaluation = {:e} %", (b_sec - &b_coarse).norm() / b_coarse.norm() * 100.0);
 }
