1a0c85fb7d683b323d523e78d4ca49a7d0131cc4
Devin <devinbalian@gmail.com>
2025-10-21 18:08:24 +0200

    implement node parsing in gmsh.rs

--- src/io/gmsh.rs
+++ src/io/gmsh.rs
@@ -4,6 +4,7 @@ use std::path::Path;
 use std::str::FromStr;
 use itertools::Itertools;
 use nalgebra::{Point, Scalar};
+use crate::cells::quad::QuadNodes;
 use crate::mesh::face_vertex::QuadVertexMesh;
 
 /// Parses a quadrilateral mesh from a file `path`
@@ -31,49 +32,105 @@ pub fn parse_gmsh_quad_mesh<T: Scalar + FromStr, const M: usize>(path: impl AsRe
     //     ...
     // $EndNodes
 
-    // Parse nodes
-    let nodes_block_start_idx = lines.iter()
+    // PARSE NODES
+    // Header
+    let nodes_start_idx = lines.iter()
         .position(|line| line.starts_with("$Nodes"))
         .expect("$Nodes must be defined");
 
-    let header = lines[nodes_block_start_idx + 1];
+    let header = lines[nodes_start_idx + 1];
     let [num_entity_bocks, num_nodes, min_node, max_node] = header.split_whitespace()
         .map(|str| str.parse::<usize>().expect("First line of $Nodes must only contain integers"))
         .collect_array()
         .expect("First line must contain exactly four integers");
 
-    dbg!(num_entity_bocks, num_nodes, min_node, max_node);
+    // Parse each node block
     let mut verts = Vec::<Point<T, M>>::with_capacity(num_nodes);
+    let mut nodes_iter = lines.iter().skip(nodes_start_idx + 2);
 
-    let mut nodes_iter = lines.iter().skip(nodes_block_start_idx + 2);
+    while let Some(header) = nodes_iter.next() {
+        // Break if end is reached
+        if *header == "$EndNodes" { break }
 
-    // todo: repeat the process below until `nodes_iter` is exhausted
-    //  - can this be done using nodes_iter.len() != 0?
-    //  - or do it using while let Some(value) = nodes_iter.next() for each header,
-    //    and then calling take to process the coordinates
+        // Parse block header
+        let [dim, entity_tag, parametric, num_nodes] = header
+            .split_whitespace()
+            .map(|str| str.parse::<usize>().expect("First line of a node block must only contain integers"))
+            .collect_array()
+            .expect("First line must contain exactly four integers");
 
-    let [dim, entity_tag, parametric, num_nodes] = nodes_iter.next()
-        .expect("")
-        .split_whitespace()
+        // Collect vertices
+        let block_verts = (&mut nodes_iter)
+            .skip(num_nodes)
+            .take(num_nodes)
+            .map(|line| {
+                let coords = line.split_whitespace()
+                    .map(|num| num.parse::<T>().expect("Vertex coordinates must be of type `T`"))
+                    .next_array::<M>() // todo: this is used instead of collect_array, to just remove the z-component. Change?
+                    .unwrap_or_else(|| panic!("Vertices must be of M = {M} dimensions"));
+
+                Point::from(coords)
+            });
+        verts.extend(block_verts);
+    }
+
+    // $Elements
+    // numEntityBlocks(size_t) numElements(size_t)
+    //  minElementTag(size_t) maxElementTag(size_t)
+    // entityDim(int) entityTag(int) elementType(int; see below)
+    //  numElementsInBlock(size_t)
+    //  elementTag(size_t) nodeTag(size_t) ...
+    // ...
+    //     ...
+    // $EndElements
+
+    // PARSE ELEMENTS
+    // Header
+    let elems_start_idx = lines.iter()
+        .position(|line| line.starts_with("$Elements"))
+        .expect("$Elements must be defined");
+
+    let header = lines[elems_start_idx + 1];
+    let [num_entity_bocks, num_elems, min_elem, max_elem] = header.split_whitespace()
         .map(|str| str.parse::<usize>().expect("First line of $Nodes must only contain integers"))
         .collect_array()
         .expect("First line must contain exactly four integers");
 
-    let vertices = nodes_iter
-        .skip(num_nodes)
-        .take(num_nodes)
-        .map(|line| {
-            let coords = line.split_whitespace()
-                .map(|num| num.parse::<T>().expect("Vertex coordinates must be of type `T`"))
-                .next_array::<M>() // todo: this is used instead of collect_array, to just remove the z-component. Change?
-                .unwrap_or_else(|| panic!("Vertices must be of M = {dim} dimensions", dim = M));
+    // Parse each element block
+    let mut quads = Vec::<QuadNodes>::with_capacity(num_elems);
+    let mut elems_iter = lines.iter().skip(elems_start_idx + 2);
 
-            Point::from(coords)
-        });
+    while let Some(header) = elems_iter.next() {
+        // Break if end is reached
+        if *header == "EndElements" { break }
+        dbg!(header);
 
-    verts.extend(vertices);
+        // fixme: find out why the below code panics
+        // Parse block header
+        let [dim, entity_tag, elem_type, num_elems] = header
+            .split_whitespace()
+            .map(|str| str.parse::<usize>().expect("First line of an element block must only contain integers"))
+            .collect_array()
+            .expect("First line must contain exactly four integers");
 
-    dbg!(verts);
+        if elem_type == 3 { println!("QUAD") }
+        dbg!(num_elems);
+        let _ = (&mut nodes_iter).take(num_elems);
+
+        // // Collect vertices
+        // let block_verts = (&mut nodes_iter)
+        //     .skip(num_nodes)
+        //     .take(num_nodes)
+        //     .map(|line| {
+        //         let coords = line.split_whitespace()
+        //             .map(|num| num.parse::<T>().expect("Vertex coordinates must be of type `T`"))
+        //             .next_array::<M>() // todo: this is used instead of collect_array, to just remove the z-component. Change?
+        //             .unwrap_or_else(|| panic!("Vertices must be of M = {M} dimensions"));
+        //
+        //         Point::from(coords)
+        //     });
+        // verts.extend(block_verts);
+    }
 
     todo!()
 }
