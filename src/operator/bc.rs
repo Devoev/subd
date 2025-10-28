5d5ca49c642131b4947c9ccfdd47821ef61ef6ed
devin <devinbalian@gmail.com>
2025-10-28 11:03:43 +0100

    implement deflate and inflate of DirichletBC for non-homogeneous BCs

--- src/operator/bc.rs
+++ src/operator/bc.rs
@@ -1,23 +1,20 @@
 use crate::cells::node::Node;
 use crate::cells::traits::{CellBoundary, CellConnectivity, OrderedCell, OrientedCell};
 use crate::mesh::elem_vertex::ElemVertexMesh;
-use itertools::Itertools;
-use nalgebra::{DVector, Point, Scalar, U2};
+use nalgebra::{ComplexField, DVector, Point, Scalar, U2};
 use nalgebra_sparse::{CooMatrix, CsrMatrix};
 use num_traits::Zero;
 use std::collections::BTreeSet;
 use std::hash::Hash;
-use std::ops::AddAssign;
 
 /// Homogeneous Dirichlet boundary conditions on nodes.
 pub struct DirichletBc<T> {
-    /// The number of total nodes.
-    num_nodes: usize,
+    /// Boundary-condition indices.
+    idx_bc: BTreeSet<usize>,
 
     /// Degrees-of-freedom indices.
     idx_dof: BTreeSet<usize>,
 
-    // todo: actually use u_bc in inflate and deflate functions
     /// Vector of solution coefficients on the boundary.
     u_bc: DVector<T>
 }
@@ -28,7 +25,7 @@ impl <T: Scalar + Zero> DirichletBc<T> {
     pub fn new(num_nodes: usize, idx_bc: BTreeSet<Node>, u_bc: DVector<T>) -> Self {
         let idx: BTreeSet<Node> = (0..num_nodes).collect();
         let idx_dof: BTreeSet<Node> = idx.difference(&idx_bc).copied().collect();
-        DirichletBc { num_nodes, idx_dof, u_bc }
+        DirichletBc { idx_bc, idx_dof, u_bc }
     }
 
     /// Constructs a new [`DirichletBc`] with *homogeneous* (= zero) boundary data.
@@ -83,40 +80,64 @@ impl <T: Scalar + Zero> DirichletBc<T> {
     pub fn num_dof(&self) -> usize {
         self.idx_dof.len()
     }
+
+    /// Returns the number of BC indices.
+    pub fn num_bc(&self) -> usize {
+        self.idx_bc.len()
+    }
+
+    /// Returns the total number of indices.
+    pub fn num_indices(&self) -> usize {
+        self.num_dof() + self.num_bc()
+    }
 }
 
-impl <T: Scalar + Zero + Copy + AddAssign> DirichletBc<T> {
+impl <T: ComplexField + Copy> DirichletBc<T> {
     /// Deflates the system `ax = b` by removing rows and columns of BC indices.
     /// Returns the reduced system matrix and right hand side vector.
     pub fn deflate(&self, a: CsrMatrix<T>, b: DVector<T>) -> (CsrMatrix<T>, DVector<T>) {
-        // Convert `self.idx_dof` to a vec
-        let num_dof = self.idx_dof.len();
-        let idx_dof = self.idx_dof.iter().copied().collect_vec();
+        // Save num DOFs/BCs
+        let num_dof = self.num_dof();
+        let num_bc = self.num_bc();
 
         // Deflate right hand side
         // let b_dof = b.remove_rows_at(&idx_bc); // todo: is this better?
-        let b_dof = b.select_rows(idx_dof.iter());
+        let b_dof = b.select_rows(self.idx_dof.iter());
 
-        // Build new COO matrix on DOF indices
+        // Build new COO matrix on DOF/BC indices
         let mut a_dof_dof = CooMatrix::new(num_dof, num_dof);
-
-        for i in 0..num_dof {
-            for j in 0..num_dof {
-                let i_dof = idx_dof[i];
-                let j_dof = idx_dof[j];
+        for (i, &i_dof) in self.idx_dof.iter().enumerate() {
+            for (j, &j_dof) in self.idx_dof.iter().enumerate() {
                 // todo: the call to index_entry / get_entry is very inefficient.
                 a_dof_dof.push(i, j, a.index_entry(i_dof, j_dof).into_value());
             }
         }
 
-        let a_dof_dof = CsrMatrix::from(&a_dof_dof);
+        let mut a_dof_bc = CooMatrix::new(num_dof, num_bc);
+        for (i, &i_dof) in self.idx_dof.iter().enumerate() {
+            for (j, &j_bc) in self.idx_bc.iter().enumerate() {
+                // todo: the call to index_entry / get_entry is very inefficient.
+                a_dof_bc.push(i, j, a.index_entry(i_dof, j_bc).into_value());
+            }
+        }
 
-        (a_dof_dof, b_dof)
+        // Conversion to CSR
+        let a_dof_dof = CsrMatrix::from(&a_dof_dof);
+        let a_dof_bc = CsrMatrix::from(&a_dof_bc);
+        (a_dof_dof, b_dof - a_dof_bc * &self.u_bc)
     }
 
     /// Inflates a vector `u` by inserting the values from `u_dof` at the DOF indices.
     pub fn inflate(&self, u_dof: DVector<T>) -> DVector<T> {
-        let mut u = DVector::zeros(self.num_nodes);
+        // Create empty coefficients vector
+        let mut u = DVector::zeros(self.num_indices());
+
+        // Fill BC values
+        for (i_local, &i) in self.idx_bc.iter().enumerate() {
+            u[i] = self.u_bc[i_local];
+        }
+
+        // Fill DOF values
         for (i_local, &i) in self.idx_dof.iter().enumerate() {
             u[i] = u_dof[i_local];
         }
