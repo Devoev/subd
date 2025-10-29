f3d37257c3551b19ff05ac445b30891263dd9ced
devin <devinbalian@gmail.com>
2025-10-29 11:32:15 +0100

    implement axial symmetry in catmull_clark_poisson_dir_axi_cylinder.rs

--- examples/catmull_clark_poisson_dir_axi_cylinder.rs
+++ examples/catmull_clark_poisson_dir_axi_cylinder.rs
@@ -9,9 +9,9 @@
 //! The discretization is performed on a 2D axisymmetric slice of the cylinder.
 
 use iter_num_tools::lin_space;
-use itertools::Itertools;
-use nalgebra::{point, DVector, Point2, Vector1};
-use nalgebra_sparse::CsrMatrix;
+use itertools::{izip, Itertools};
+use nalgebra::{point, DMatrix, DVector, Dyn, Matrix2, OMatrix, Point2, Vector1, U2};
+use nalgebra_sparse::{CooMatrix, CsrMatrix};
 use num_traits::Zero;
 use std::collections::BTreeSet;
 use subd::cells::quad::QuadNodes;
@@ -22,14 +22,16 @@ use subd::element::traits::Element;
 use subd::error::l2_error::L2Norm;
 use subd::mesh::face_vertex::QuadVertexMesh;
 use subd::operator::bc::DirichletBc;
-use subd::operator::laplace::Laplace;
 use subd::operator::linear_form::LinearForm;
 use subd::plot::plot_fn_msh;
 use subd::quadrature::pullback::PullbackQuad;
 use subd::quadrature::tensor_prod::GaussLegendreBi;
-use subd::subd::catmull_clark::basis::CatmarkBasis;
+use subd::quadrature::traits::Quadrature;
+use subd::space::eval_basis::EvalGrad;
+use subd::space::Space;
+use subd::subd::catmull_clark::basis::{CatmarkBasis, CatmarkPatchBasis};
 use subd::subd::catmull_clark::mesh::CatmarkMesh;
-use subd::subd::catmull_clark::patch::CatmarkPatchNodes;
+use subd::subd::catmull_clark::patch::{CatmarkPatch, CatmarkPatchNodes};
 use subd::subd::catmull_clark::quadrature::SubdUnitSquareQuad;
 use subd::subd::catmull_clark::space::CatmarkSpace;
 
@@ -37,7 +39,7 @@ fn main() {
     // Define problem
     let u = |p: Point2<f64>| Vector1::new(p.y); // Voltage at top and bottom plates
     let u = |p: Point2<f64>| Vector1::new(p.x*1.25 - 0.25); // Voltage at inner and outer hulls (linear version)
-    // let u = |p: Point2<f64>| Vector1::new(p.x.ln() / 5f64.ln()); // Voltage at inner and outer hulls (log version) todo: is this correct?
+    let u = |p: Point2<f64>| Vector1::new((p.x * 5.0).ln() / 5f64.ln()); // Voltage at inner and outer hulls (log version)
 
     let r0 = 0.2;
     let coords_square = vec![
@@ -50,7 +52,7 @@ fn main() {
     // Define mesh
     let quads = vec![QuadNodes::new(0, 1, 2, 3)];
     let mut quad_msh = QuadVertexMesh::new(coords_square, quads);
-    quad_msh = quad_msh.catmark_subd().unpack();
+    quad_msh = quad_msh.catmark_subd().catmark_subd().catmark_subd().catmark_subd().unpack();
     let msh = CatmarkMesh::from(quad_msh);
 
     // Solve problem
@@ -72,10 +74,9 @@ fn solve(msh: &CatmarkMesh<f64, 2>, u: impl Fn(Point2<f64>) -> Vector1<f64>) -> 
     let quad = PullbackQuad::new(SubdUnitSquareQuad::new(ref_quad, 1));
 
     // Assemble system
-    let laplace = Laplace::new(msh, &space);
     let form = LinearForm::new(msh, &space, |_p| Vector1::zero());
-    let k_coo = laplace.assemble(&quad);
-    let f = form.assemble(&quad);
+    let k_coo = assemble_axi_2d(msh, &space, &quad);
+    let f = form.assemble(&quad); // todo: this is only correct, because f = 0. Otherwise, implement axial symmetric
     let k = CsrMatrix::from(&k_coo);
 
     // Calculate boundary info
@@ -125,4 +126,77 @@ fn solve(msh: &CatmarkMesh<f64, 2>, u: impl Fn(Point2<f64>) -> Vector1<f64>) -> 
     }).show();
 
     (space.dim(), err_l2, norm_l2)
+}
+
+/// Quadrature rule for this example.
+type CatmarkQuadrature = PullbackQuad<SubdUnitSquareQuad<f64, GaussLegendreBi, 2>>;
+
+/// Assembles the stiffness matrix for the axial-symmetric geometry.
+fn assemble_axi_2d(msh: &CatmarkMesh<f64, 2>, space: &CatmarkSpace<f64, 2>, quad: &CatmarkQuadrature) -> CooMatrix<f64> {
+    // Create empty matrix
+    let mut kij = CooMatrix::zeros(space.dim(), space.dim());
+
+    // Iteration over all mesh elements
+    for (elem, cell) in msh.elem_cell_iter() {
+        // Build local space and local stiffness matrix
+        let (sp_local, idx) = space.local_space_with_idx(cell);
+        let kij_local = assemble_axi_2d_local(&elem, &sp_local, quad);
+
+        // Fill global stiffness matrix with local entries
+        let idx_local_global = idx.enumerate();
+        for ((i_local, i), (j_local, j)) in idx_local_global.clone().cartesian_product(idx_local_global) {
+            kij.push(i, j, kij_local[(i_local, j_local)]);
+        }
+    }
+
+    kij
+}
+
+/// Assembles the local stiffness matrix for the axial-symmetric geometry.
+fn assemble_axi_2d_local(elem: &CatmarkPatch<f64, 2>, sp_local: &Space<f64, CatmarkPatchBasis>, quad: &CatmarkQuadrature) -> DMatrix<f64> {
+    let ref_elem = elem.parametric_element();
+    let geo_map = elem.geo_map();
+
+    // Collect radii
+    let buf_radii: Vec<_> = quad.nodes_ref::<f64, CatmarkPatch<f64, 2>>(&ref_elem)
+        .map(|p| geo_map.eval(p).x)
+        .collect();
+
+    // Evaluate gradients
+    let buf_grads: Vec<_> = quad.nodes_ref::<f64, CatmarkPatch<f64, 2>>(&ref_elem)
+        .map(|p| sp_local.basis.eval_grad(p))
+        .collect();
+
+    // Evaluate inverse gram matrices
+    let buf_g_inv: Vec<_> = quad.nodes_ref::<f64, CatmarkPatch<f64, 2>>(&ref_elem)
+        .map(|p| {
+            let j = geo_map.eval_diff(p);
+            (j.transpose() * j).try_inverse().unwrap()
+        })
+        .collect();
+
+    // Calculate pullback of product grad_u * grad_v
+    let gradu_gradv_pullback = |grad_b: &OMatrix<f64, U2, Dyn>, g_inv: &Matrix2<f64>, i: usize, j: usize| {
+        // Get gradients
+        let grad_bi = grad_b.column(i);
+        let grad_bj = grad_b.column(j);
+
+        // Calculate integrand
+        (grad_bi.transpose() * g_inv * grad_bj).x
+    };
+
+    // Integrate over all combinations of grad_b[i] * grad_b[j] and integrate
+    let num_basis = sp_local.dim();
+    let kij = (0..num_basis).cartesian_product(0..num_basis)
+        .map(|(i, j)| {
+            // Integrand for stiffness matrix - Add factor of `rho` to fix Jacobian in cylindrical coordinates
+            let integrand = izip!(&buf_grads, &buf_g_inv, &buf_radii)
+                .map(|(b_grad, g_inv, rho)| rho * gradu_gradv_pullback(b_grad, g_inv, i, j));
+
+            // Perform integration
+            quad.integrate_elem(elem, integrand)
+        });
+
+    // Assemble matrix
+    DMatrix::from_iterator(num_basis, num_basis, kij)
 }
